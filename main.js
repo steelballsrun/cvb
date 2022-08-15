5d59141910632254ba63412b10daf743d51b40a2
steelballsrun <87947152+steelballsrun@users.noreply.github.com>
2022-08-14 20:35:07 -0400

    Add files via upload

---
+++ main.js
@@ -0,0 +1,22 @@
+function setup(){
+canvas=createCanvas(550,550);
+canvas.position(560,150);
+video=createCapture(VIDEO);
+video.size(550,500);
+poseNet=ml5.poseNet(video,modelLoaded);
+poseNet.on('pose',gotPoses);
+}
+
+function modelLoaded(){
+console.log('poseNet is initialized');
+}
+
+function gotPoses(results){
+if(results.length>0){
+console.log(results);
+}
+}
+
+function draw(){
+background('#e80756');
+}
